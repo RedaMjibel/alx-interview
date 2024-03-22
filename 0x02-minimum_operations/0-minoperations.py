7dfb90257d37feebfdcc3db103e7407bcdc4be7a
reda mjibel <reda_mjibel@hotmail.com>
2024-03-22 02:58:30 +0000

    test

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 0-minoperations"""
+
+
+def minOperations(n):
+    """
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    if n < 2:
+        return 0
+
+    ops = 0
+    divisor = 2
+
+    while divisor <= n:
+        if n % divisor == 0:
+            ops += divisor
+            n //= divisor
+        else:
+            divisor += 1
+
+    return ops

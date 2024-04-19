93d9a07489a774295d038b732da0300c61750a0a
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-18 17:33:53 -0700

    semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,40 +2,39 @@
 
 const request = require('request');
 
-const url = "https://swapi-api.hbtn.io/api/films/";
+const url = 'https://swapi-api.hbtn.io/api/films/';
 const movieId = process.argv[2];
 
 request(url + movieId, function (err, res, body) {
-    if (err) {
-        console.error('Error:', err);
-        return;
-    }
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
 
-    if (res.statusCode !== 200) {
-        console.error('Status:', res.statusCode);
-        return;
-    }
+  if (res.statusCode !== 200) {
+    console.error('Status:', res.statusCode);
+    return;
+  }
 
-    const actors = JSON.parse(body).characters;
-    exactOrder(actors, 0);
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
 
 const exactOrder = (actors, x) => {
-    if (x === actors.length) return;
+  if (x === actors.length) return;
 
-    request(actors[x], function (err, res, body) {
-        if (err) {
-            console.error('Error:', err);
-            return;
-        }
+  request(actors[x], function (err, res, body) {
+    if (err) {
+      console.error('Error:', err);
+      return;
+    }
 
-        if (res.statusCode !== 200) {
-            console.error('Status:', res.statusCode);
-            return;
-        }
+    if (res.statusCode !== 200) {
+      console.error('Status:', res.statusCode);
+      return;
+    }
 
-        console.log(JSON.parse(body).name);
-        exactOrder(actors, x + 1);
-    });
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
 };
-

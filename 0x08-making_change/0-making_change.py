0968811cc86cb76fa93a6ac4135b94ddfa11e8bf
Reda Mjibel <reda_mjibel@hotmail.com>
2024-05-02 23:42:40 +0000

    fix syntax

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -18,4 +18,4 @@ def makeChange(coins, total):
         for i in range(coin, total):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return dp[-1] if dp[-1] = float('inf') else -1
+    return dp[-1] if dp[-1] != float('inf') else -1
